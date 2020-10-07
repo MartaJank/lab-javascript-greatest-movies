d03a5d06ae73270275211a6488c0ac876d3770bd
Marta Jankowska <martajan218@gmail.com>
2020-10-07 18:56:30 +0200

    full lab done with bonuses

--- src/movies.js
+++ src/movies.js
@@ -22,7 +22,11 @@ howManyMovies(movies);
 
 function ratesAverage(movies) {
     let avrg = movies.reduce(function(acc, currentValue) {
-      return acc + currentValue.rate / movies.length
+      if (currentValue.rate){
+        return acc + currentValue.rate / movies.length
+      } else {
+        return acc;
+      }
     }, 0)
     return Number(avrg.toFixed(2))
   }
@@ -45,7 +49,12 @@ function dramaMoviesRate(movies) {
 
 function orderByYear(movies) {
     let order = movies.sort(function(a, b) {
-      return a.year - b.year
+      if(a.year === b.year) {
+        return a.title.localeCompare(b.title);
+      } else {
+        return a.year - b.year
+      }
+      
     })
     return order.concat()
   }
@@ -57,12 +66,83 @@ function orderByYear(movies) {
 function orderAlphabetically(movies) {
     let titles = movies.map(function(e) {
         return e.title;
-    }) 
-    return [...new Set(titles)];
+    })
+    if (movies.length > 20) {
+      return [...new Set(titles)].sort().slice(0, 20);
+    } else {
+      return [...new Set(titles)].sort();
+    }
 }
 
-orderAlphabetically(movies).sort();
+orderAlphabetically(movies);
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+function turnHoursToMinutes(movies) {
+
+  return movies.slice(0).map(function(movie) {
+      let timeString = movie.duration
+      let splitString = timeString.split(' ')
+      let timeHours = 0;
+      let timeMinutes = 0;
+
+      if (splitString.length === 1) {
+          if (splitString[0].includes('h')) {
+              timeHours = splitString[0].replace('h', '') * 60
+          } else if (splitString[0].includes('min')) {
+              timeMinutes = splitString[0].replace('min', '') * 1
+          }
+      } else {
+          timeHours = splitString[0].replace('h', '') * 60
+          timeMinutes = splitString[1].replace('min', '') * 1
+      }
+
+      let totalTimeMinutes = timeHours + timeMinutes
+      return { duration: totalTimeMinutes }
+  })
+}
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+function bestYearAvg(movies) {
+  if (movies.length === 0) {
+      return null
+  }
+
+  let rating = 0;
+  let yearRates = {}
+
+  movies.forEach(movie => {
+      yearRates[movie.year] ? yearRates[movie.year].push(movie.rate) : (yearRates[movie.year] = [], yearRates[movie.year].push(movie.rate))
+  })
+
+  Object.keys(yearRates).forEach(function(rate) {
+      let total = 0;
+      let count = 0;
+      if (yearRates[rate].length > 0) {
+          for (x = 0; x < yearRates[rate].length; x++){
+              total += yearRates[rate][x]
+              count++
+          }
+      }
+      yearRates[rate] = Number(parseFloat(total / count).toFixed(2))
+  })
+
+  let highest = 0
+  let year = 0;
+  let avgRate = 0;
+
+  Object.keys(yearRates).forEach(function(element) {
+      if (yearRates[element] > highest) {
+          highest = yearRates[element]
+          year = element
+          avgRate = yearRates[element]
+      } else if (yearRates[element] == highest) {
+          if (year > element) {
+              year = element 
+          }
+      }
+  })
+
+  return `The best year was ${year} with an average rate of ${avgRate}`
+}
+
+bestYearAvg(movies)
